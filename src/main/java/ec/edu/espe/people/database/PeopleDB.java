2be8439716da9aec6ba01766ede053341ed7d146
steevenm888 <steevenm888@gmail.com>
2021-03-19 16:24:25 -0500

    minor changes

--- src/main/java/ec/edu/espe/people/database/PeopleDB.java
+++ src/main/java/ec/edu/espe/people/database/PeopleDB.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.espe.people.model;
+package ec.edu.espe.people.database;
 
+import ec.edu.espe.people.model.People;
 import java.util.ArrayList;
 import lombok.Data;
 
